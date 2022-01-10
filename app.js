dfa9ece16cdba7dfe564ec775680318b89f77d67
LaviniaRus <lavinia.rus20@gmail.com>
2022-01-10 19:08:35 +0200

    connection fix

--- app.js
+++ app.js
@@ -16,9 +16,34 @@ const connection = mysql.createConnection({
     database: 'heroku_2dbe65ac07f9310'
 });
 
-connection.connect(function() {
-    console.log("Database connected successfully");
-});
+
+function handleDisconnect() {
+  
+    connection.connect(function(err) {              
+      if(err) {                                     
+        console.log('error when connecting to db:', err);
+        setTimeout(handleDisconnect, 2000); 
+      } 
+      else {
+        console.log("Database connected successfully");
+      }                                   
+    });                                     
+
+    connection.on('error', function(err) {
+      console.log('db error', err);
+      if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
+        handleDisconnect();                     
+      } else {                                  
+        throw err;                                 
+      }
+    });
+  }
+
+  setInterval(function () {
+connection.query('SELECT 1');
+}, 5000);
+  
+handleDisconnect();
 
 app.set('view engine', 'ejs');
 app.use(cookieParser()); 
